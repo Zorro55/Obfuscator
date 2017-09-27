cdb3baaabd11134679af929d148520a8e62dcdee
Andrej <andrej@vistadraft.com>
2017-09-27 15:48:15 -0700

    Vs project files

---
+++ dotfuscatorproject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotfuscatorproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World2!");
+            Console.WriteLine("Hello World3!");
+            Console.WriteLine("Hello World4!");
+            Console.WriteLine("Hello World5!");
+            Console.WriteLine("Hello Worl6d!");
+            Console.WriteLine("Hello World7!");
+
+            Console.ReadKey();
+        }
+    }
+}
